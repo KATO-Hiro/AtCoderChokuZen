d8a43dba983614ff26c8161ee28b8520085d84ef
Kato Hiroki <k.hiro1818@gmail.com>
2020-06-14 07:19:38 +0000

    Enable to calc time remaining

--- tests/test_schedule.py
+++ tests/test_schedule.py
@@ -3,10 +3,54 @@ from datetime import timedelta
 from datetime import timezone
 import pytest
 
+from bot.schedule import calc_time_remaining
 from bot.schedule import set_announce_time
 
 
 class TestSchedule(object):
+
+    @pytest.mark.parametrize((
+        'contest_day', 'contest_hour', 'contest_minute',
+        'now_day', 'now_hour', 'now_minute',
+        'expected_remain_hours', 'expected_remain_minutes'
+        ), [
+        (14, 21, 0,
+         14, 21, 0,
+         0, 0),
+        (14, 21, 0,
+         14, 12, 0,
+         9, 0),
+        (14, 20, 0,
+         14, 12, 0,
+         8, 0),
+        (14, 21, 0,
+         13, 21, 30,
+         23, 30),
+        (14, 21, 30,
+         14, 12, 0,
+         9, 30),
+        (14, 21, 15,
+         14, 12, 3,
+         9, 12),
+    ])
+    def test_calc_time_remaining(self,
+                                 contest_day, contest_hour, contest_minute,
+                                 now_day, now_hour, now_minute,
+                                 expected_remain_hours, expected_remain_minutes
+                                 ):
+        remain_hours, remain_minutes = calc_time_remaining(
+            contest_start_time=self._generate_datetime_jst(
+                year=2020, month=6,
+                day=contest_day, hour=contest_hour, minute=contest_minute
+            ),
+            now_jst=self._generate_datetime_jst(
+                year=2020, month=6,
+                day=now_day, hour=now_hour, minute=now_minute
+            )
+        )
+
+        assert remain_hours == expected_remain_hours
+        assert remain_minutes == expected_remain_minutes
 
     @pytest.mark.parametrize((
         'before_hours', 'announce_start_day', 'announce_start_hour'), [
@@ -36,3 +80,16 @@ class TestSchedule(object):
 
         assert contest_start_time == expected_contest_start_time
         assert announce_start_time == expected_announce_start_time
+
+    def _generate_datetime_jst(self, year, month, day, hour, minute):
+        time_difference_hours = 9
+        minutes = 60
+        seconds = 60
+        time_difference_seconds = time_difference_hours * minutes * seconds
+
+        datetime_jst = datetime(
+            year, month, day, hour, minute,
+            tzinfo=timezone(timedelta(seconds=time_difference_seconds))
+        )
+
+        return datetime_jst
